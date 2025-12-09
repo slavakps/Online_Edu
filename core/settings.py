b5000b68a89967315564d869259dc880be14dbe4
Your Name <you@example.com>
2025-12-09 15:45:07 +0800

    в settings.py указал все настройки почтового сервиса

--- core/settings.py
+++ core/settings.py
@@ -175,6 +175,9 @@ CELERY_BEAT_SCHEDULE = {
     },
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
