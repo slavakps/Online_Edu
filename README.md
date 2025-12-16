44914f8b46e593164837cfb029952acc0428501c
Your Name <you@example.com>
2025-12-16 19:53:24 +0800

    Описал файл docker-compose для запуска всех частей проекта единой командой.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Запуск с помощью Docker Compose
+
+В корне проекта выполните:
+
+docker-compose up --build
--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ry@6=!=8ppy9f&cumpgd&cfm0+4ct#e!xm&m967c%h_#9h2$87'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-key-change-this')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
